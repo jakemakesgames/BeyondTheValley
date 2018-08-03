102af9b0dc44887c444bfcce342185de979012c3
jakemakesgames <jakebrown.designer@gmail.com>
2018-08-03 15:37:26 +1000

    created a new "playground" scene to experiment and test in. base player controller functionality implemented. (Can use WASD/ Arrow Keys)

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rb2D;
+
+	public float movementSpeed;
+	private Vector2 moveVelocity;
+
+	void Start(){
+		// The rb2D varible is set the to Rigidbody 2D component on the Player GameObject
+		rb2D = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update(){
+		// The moveInput Vector2 is equal to a new Vector2 - The Horizontal Axis (for X axis movement), and The Vertical Axis (for Y axis movement)
+		// Left = -1 | Right = 1 | Up = 1 | Down = -1 (Use GetAxisRaw for snappier movements)
+		Vector2 moveInput = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		// Set the moveVelocity equal to the moveInput multiplied by the movementSpeed variable (Normalized so the speed is the same in all directions)
+		moveVelocity = moveInput.normalized * movementSpeed;
+	}
+
+	void FixedUpdate(){
+		// Move the Player
+		rb2D.MovePosition (rb2D.position + moveVelocity * Time.fixedDeltaTime);
+	}
+}
