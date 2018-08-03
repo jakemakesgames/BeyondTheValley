aec1ad290adebd96161c5602d75d7a61bc0a7f4b
jakemakesgames <jakebrown.designer@gmail.com>
2018-08-03 17:47:57 +1000

    Projectile Script functional, working on fixing the cursor position stopping bug

---
+++ Assets/Scripts/Player/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	private Vector2 target;
+	// The movement speed of the projectile
+	public float speed;
+	// The damage amount the projectile inflicts
+	public int damageAmount;
+	// The score amount the player recieves when the projectile collides with an enemy
+	public int scoreAmount;
+
+	private bool canDestroy;
+
+	void Start(){
+		target = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		canDestroy = true;
+	}
+
+	void Update(){
+		// Move the Projectile towards the cursor
+		transform.position = Vector2.MoveTowards(transform.position, target + 100f, speed * Time.deltaTime);
+
+	}
+
+
+}
