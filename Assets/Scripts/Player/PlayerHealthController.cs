7e017d0ff1379a83fcd92dc453d612c4d0aca5fc
jakemakesgames <jakebrown.designer@gmail.com>
2018-08-04 17:48:47 +1000

    Added a player health controller script and set up basic health (hearts) UI, which is all hooked up and functioning in editor

---
+++ Assets/Scripts/Player/PlayerHealthController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthController : MonoBehaviour {
+
+	// The amount of health the player has
+	public int health;
+	// The total amount of health the player has
+	public int numberOfHearts;
+	// The Array of Images
+	public Image[] hearts;
+	// The Sprite of the Full Heart
+	public Sprite fullHeart;
+	// The Sprite of the Empty Heart
+	public Sprite emptyHeart;
+
+	void Update(){
+		
+		#region Health Cap
+		if (health > numberOfHearts) {
+			health = numberOfHearts;
+		}
+		#endregion
+
+		#region Update the UI
+		// This for loop updates the UI elements
+		for (int i = 0; i < hearts.Length; i++) {
+
+			// The hearts that the player is missing will not have the full heart sprite
+			if (i < health) {
+				hearts [i].sprite = fullHeart;
+			} else {
+				hearts [i].sprite = emptyHeart;
+			}
+
+			// This ensures the max amount of hearts on the screen cannot be greater than the numberOfHearts variable
+			if (i < numberOfHearts) {
+				hearts [i].enabled = true;
+			} else {
+				hearts [i].enabled = false;
+			}
+		}
+		#endregion
+	}
+
+}
