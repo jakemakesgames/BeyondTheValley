7688144277778a09ce30ceab016dd4c2d4ac5e74
jakemakesgames <jakebrown.designer@gmail.com>
2018-08-05 21:49:44 +1000

    Health pick ups now prefabbed. Created an Enemy Scriptable object and set up the base enemy controller script. So far, everything is working as intended

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	public EnemyScriptable eS;
+
+	[SerializeField] private enum type{ neutral, ranged, aggressive };
+	[SerializeField] private type enemyType;
+	public int health;
+
+	void Start(){
+		// Call the EnemyTypeCheck function
+		EnemyTypeCheck ();
+		health = eS.enemyHealth;
+	}
+
+	// This Function serves the purpose of checking which type this enemy is - this will set the variable values and type specific parameters
+	void EnemyTypeCheck(){
+		if (enemyType == type.neutral) {
+			// Check if the type of this enemy is Neutral
+			Debug.Log ("I am Neutral");
+
+			// Do the Neutral Enemy Stuff here
+
+		} else if (enemyType == type.ranged) {
+			// Check if the type of this enemy is Ranged
+			Debug.Log ("I am Ranged");
+
+			// Do the Ranged Enemy Stuff here
+
+		} else if (enemyType == type.aggressive) {
+			// Check if the type of this enemy is Aggressive
+			Debug.Log ("I am Aggressive");
+
+			// Do the Aggressive Enemy Stuff here
+		}
+	}
+
+}
---
+++ Assets/Scripts/Enemy/Scriptables/EnemyScriptable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Enemy", menuName = "Enemy")]
+public class EnemyScriptable : ScriptableObject {
+
+	// The Enemy type
+	public enum enemyType { neutral, ranged, aggressive }; 
+
+	// The enemy GFX
+	public Sprite enemySprite;
+
+	// The Enemy Health Variable
+	public int enemyHealth;
+	// The Damage Amount dealth
+	public int enemyDamageAmount;
+
+	// Movement Variables
+	public float enemyMovementSpeed;
+	public float enemyMoveRange;
+
+	// How many points the Player scores when they kill the Enemy
+	public int enemyScoreValue;
+
+	// Worry about Item Drops later
+
+
+
+}
