9fb6bc32b60879573d28518b831f0a0ff39df2ab
jakemakesgames <jakebrown.designer@gmail.com>
2018-08-14 20:38:06 +1000

    base functionality for the rapid fire pickup is working as intended, tweaking will be needed.

---
+++ Assets/Scripts/Pickups/RapidFire.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RapidFire : MonoBehaviour {
+
+	PlayerController playerController;
+	public float rfTimeBetweenShots;
+
+	void Awake(){
+		// Getting a reference to the Player Controller variable
+		playerController = FindObjectOfType<PlayerController> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		// If the other game object's tag is "Player"
+		if (other.tag == "Player") {
+			// Set the player's time between shots equal to the rapid fire time between shots
+			playerController.timeBetweenShots = rfTimeBetweenShots;
+			// Destroy this game object
+			Destroy (gameObject);
+		}
+	}
+}
