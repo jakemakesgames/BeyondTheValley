653f868e4b8117098e47061bbe84224b60c02e1b
jakemakesgames <jakebrown.designer@gmail.com>
2018-08-14 20:20:12 +1000

    health pickups working, sacred heart pickup (+ 10) hearts working now as intended as well

--- Assets/Scripts/Pickups/HealthPickup.cs
+++ Assets/Scripts/Pickups/HealthPickup.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// THIS HEALTH PICK UP IS ESSENTIALLY A HP (+1) PICKUP, IT WILL GIVE THE PLAYER +1 HEART TO FILL WHAT THEY HAVE MISSING -> IF THEY HAVE FULL HEARTS THEY CANNOT PICK
+// THE HEART UP
+
 public class HealthPickup : MonoBehaviour {
 
 	PlayerHealthController playerHealthController;
 	public int healthGained;
+
+	int randNum;
 
 	void Awake(){
 		// Getting a reference to the Player Health Controller variable
@@ -15,13 +20,44 @@ public class HealthPickup : MonoBehaviour {
 	void OnTriggerEnter2D (Collider2D other){
 		// If the object collided has a tag "Play" -> Do the thing
 		if (other.tag == "Player") {
-			// Check if the player's health is less that the number of hearts, if true
-			if (playerHealthController.health < playerHealthController.numberOfHearts) {
-				// Add the healthGained int to the player's health
+
+			if (healthGained == 10) {
+				playerHealthController.health = 10;
+				playerHealthController.numberOfHearts = 10;
 				playerHealthController.GainHealth (healthGained);
 				// Destroy this game object
 				Destroy (gameObject);
 			}
+
+
+			// Check if the player's health is less that the number of hearts, if true
+			if (playerHealthController.health < playerHealthController.numberOfHearts) {
+				playerHealthController.GainHealth (healthGained);
+				// Destroy this game object
+				Destroy (gameObject);
+			}
+		}
+	}
+
+	public void HealthGainedValue(){
+		
+
+		randNum = Random.Range (1, 3);
+
+		// If the random number variable is equal to 1
+		if (randNum == 1) {
+			// Add the healthGained int to the player's health
+			healthGained = 1;
+			Debug.Log("HP UP (+" + healthGained + ")");
+		}
+		// If the random number variable is equal to 1
+		if (randNum == 2){
+			healthGained = 3;
+			Debug.Log("HP UP (+" + healthGained + ")");
+		}
+		// If the random number variable is equal to 3 ----> SACRED HEART! <----
+		if (randNum == 3) {
+			// SACRED HEART
 		}
 	}
 }
