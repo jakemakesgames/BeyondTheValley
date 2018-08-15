5fc255b60d648a038404bc652a7f2281fca8bf24
jakemakesgames <jakebrown.designer@gmail.com>
2018-08-15 22:03:34 +1000

    Bombs can now be picked up (not via enemy drops)

---
+++ Assets/Scripts/Pickups/BombPickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombPickup : MonoBehaviour {
+
+	PlayerController playerController;
+
+	void Start(){
+		playerController = FindObjectOfType<PlayerController> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player") {
+			playerController.bombCount++;
+			Debug.Log ("Bomb Up + 1");
+			Destroy (gameObject);
+		}
+	}
+}
