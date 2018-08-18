19e13677635a811eb048127eec2f23b4ee950d04
jakemakesgames <jakebrown.designer@gmail.com>
2018-08-18 12:59:59 +1000

    Script created to spawn a random enemy prefab from a list at a spawn point's location (at the moments it's a ranged AND an aggressive enemy type.)

---
+++ Assets/Scripts/Enemy/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject[] enemies;
+
+	void Start(){
+		// Call  the SpawnEnemy function
+		SpawnEnemy();
+	}
+
+	void SpawnEnemy(){
+		// Instantiate a random enemy prefab at this gameObjects position
+		Instantiate(enemies[Random.Range(0, enemies.Length)], transform.position, Quaternion.identity);
+	}
+}
