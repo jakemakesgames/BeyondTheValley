e99d047a2314ba9f882748551de2d9037ef8ec65
jakemakesgames <jakebrown.designer@gmail.com>
2018-09-05 00:27:15 +1000

    new placeholder UI sprites have been imported, sliced and played in the scene. third party typeface (and the license) has been imported, typeface used for the canvas. foliage templates created and hooked up to the foliage generator.

---
+++ Assets/Scripts/Managers/Generation/ObjGeneration.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// THIS NAMESPACE IS ONLY TEMPORARY AND FOR TESTING PURPOSES >>> REMOVE LATER <<<
+using UnityEngine.SceneManagement;
+
+public class ObjGeneration : MonoBehaviour {
+
+	[SerializeField] private string sceneToLoad;
+	public GameObject[] objectsToSpawn;
+
+	void Start(){
+		// Call the GenerateObject function
+		GenerateObject ();
+	}
+
+	void Update(){
+		// If the G key is pressed, reload the scene (test purpose only)
+		if (Input.GetKeyDown (KeyCode.G)) {
+			SceneManager.LoadScene (sceneToLoad);
+		}
+	}
+
+	// This function handles the instantiation of the object
+	void GenerateObject(){
+		int rand = Random.Range (0, objectsToSpawn.Length);
+		// Instantiate a random object from the objectsToSpawn array at this spawner's transform.position with NO rotation
+		Instantiate (objectsToSpawn [rand], transform.position, Quaternion.identity);
+		// Destroy this gameObject to prevent cluttering the scene with empty gameObjects
+
+	}
+}
