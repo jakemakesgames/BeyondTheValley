7f6a8d6457cd5ad292f61badbe9d5ac78cfbd1e2
jakemakesgames <jakebrown.designer@gmail.com>
2018-09-21 14:46:17 +1000

    created an option menu / cript and hooked it all up. quality settings dropdown, master vol slider and resolution working, fullscreen NOT working STRATA asset removed

---
+++ Assets/Scripts/Managers/Menu/MenuManager.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+
+	[Header("Temp Intro")]
+	// Temp story panel
+	public GameObject introCanvas;
+	public GameObject titleScreenCanvas;
+	[SerializeField] private bool pressedSpacebar;
+
+	/*public Text introText;
+	public string[] sentences;
+	private int index;
+	public float typingSpeed;*/
+
+	[Header("Main Menu Variables")]
+	// Canvas Variables
+	public GameObject menuCanvas;
+	public GameObject controlsCanvas;
+	public GameObject optionsCanvas;
+	public GameObject quitCanvas;
+
+	void Start(){
+		
+		#region Setting Correct Panels
+		// Setting the correct Canvases to Active or Not
+
+		introCanvas.SetActive(true);
+		titleScreenCanvas.SetActive (false);
+
+
+		menuCanvas.SetActive (true);
+		controlsCanvas.SetActive (false);
+		optionsCanvas.SetActive (false);
+		quitCanvas.SetActive (false);
+		#endregion
+	}
+
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Space) && !pressedSpacebar) {
+			titleScreenCanvas.SetActive (false);
+			pressedSpacebar = true;
+
+		}
+	}
+
+	public void PlayGame(){
+		// Change to Game Scene
+	}
+
+	public void Back(){
+		#region IF MenuActive is TRUE
+		menuCanvas.SetActive (true);
+		controlsCanvas.SetActive (false);
+		optionsCanvas.SetActive (false);
+		quitCanvas.SetActive (false);
+		#endregion
+	}
+
+	public void ControlsMenu(){
+		#region IF ControlsActive is TRUE
+		menuCanvas.SetActive (false);
+		controlsCanvas.SetActive (true);
+		optionsCanvas.SetActive (false);
+		quitCanvas.SetActive (false);
+		#endregion
+	}
+
+	public void OptionsMenu(){
+		#region IF OptionsActive is TRUE
+		menuCanvas.SetActive (false);
+		controlsCanvas.SetActive (false);
+		optionsCanvas.SetActive (true);
+		quitCanvas.SetActive (false);
+		#endregion
+	}
+
+	public void QuitMenu(){
+		#region IF QuitActive is TRUE
+		menuCanvas.SetActive (false);
+		controlsCanvas.SetActive (false);
+		optionsCanvas.SetActive (false);
+		quitCanvas.SetActive (true);
+		#endregion
+	}
+
+	public void ConfirmQuit(){
+		#region Confirm Quit
+		// Quit the game
+		//UnityEditor.EditorApplication.isPlaying = false;
+		Application.Quit ();
+		#endregion
+	}
+		
+	public void SkipIntro(){
+		#region Skip the Plaveholder Intro
+		// Set the intro canvas to false (hiding it from view).
+		introCanvas.SetActive(false);
+		pressedSpacebar = false;
+		titleScreenCanvas.SetActive (true);
+		#endregion
+	}
+
+
+
+}
---
+++ Assets/Scripts/Managers/Menu/SettingsManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour {
+
+	[Header("Audio Components")]
+	// Master Volume Audio Mixer
+	public AudioMixer audioMixer;
+
+	[Header("Resolution Components")]
+	public Dropdown resolutionsDropdown;
+	// Resolutions Array
+	private Resolution[] resolutions;
+
+	void Start(){
+		
+		resolutions = Screen.resolutions;
+		// Clear all of the possible resolution options from the dropdown
+		resolutionsDropdown.ClearOptions();
+
+		// Turn the array of resolutions into a list of strings
+		List<string> resOptions = new List<string>();
+
+		// Set a current resolution int
+		int currentResolutionIndex = 0;
+		for (int i = 0; i < resolutions.Length; i++) {
+			// for every resolution in the list, create a string with that resolution's width X height
+			string resOption = resolutions[i].width + " x " + resolutions[i].height;
+			// Add the string to the resOptions list
+			resOptions.Add(resOption);
+
+			// if the Ith element of resolutions is equal to the screen's current resolution
+			if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height) {
+				//Set the current resolution index to this
+				currentResolutionIndex = i;
+			}
+		}
+
+		// Add the resolutions from the array into the dropdown field
+		resolutionsDropdown.AddOptions(resOptions);
+		// Set the resolutionDropdown's value
+		resolutionsDropdown.value = currentResolutionIndex;
+		// Refresh the shown value
+		resolutionsDropdown.RefreshShownValue();
+
+	}
+
+	// SET VOLUME
+	public void SetVolume(float mainVolume){
+		// Set the main game's volume
+		audioMixer.SetFloat("MainVol", mainVolume);
+		//Debug.Log("Volume: " + volume);
+	}
+
+	// SET THE QUALITY SETTINGS
+	public void SetQualitySettings(int qualityIndex){
+		// Set the game's quality settings
+		QualitySettings.SetQualityLevel(qualityIndex);
+	}
+
+	// TOGGLE FULLSCREEN
+	public void SetFullscreen (bool isFullscreen){
+		Screen.fullScreen = isFullscreen;
+	}
+
+	// SET THE WINDOW RESOLUTION
+	public void SetResolution(int resolutionIndex){
+		Resolution resolution = resolutions [resolutionIndex];
+		Screen.SetResolution (resolution.width, resolution.height, Screen.fullScreen);
+	}
+}
