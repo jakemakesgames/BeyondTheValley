6168483c3d3c652e72c775bdffe4caf48196d460
jakemakesgames <jakebrown.designer@gmail.com>
2018-10-04 22:58:41 +1000

    game feel worked on. Trees now animate slightly to 'sawy' in the wind. grass also rustles when the player walks through it. new tree prefabs created to accomodate these new additions.

---
+++ Assets/Scripts/Environment/EnvironmentMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentMovement : MonoBehaviour {
+
+	private Animator anim;
+
+	void Start(){
+		anim = GetComponentInChildren<Animator> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player") {
+			anim.SetTrigger ("Move");
+		}
+	}
+}
